b51e5e5fcca485cb0bd06e46fb54d8fff859e8a8
serpenti <sergeitarabukin@yandex.ru>
2025-08-20 01:04:54 +0300

    feat(NavigationPanel&&Route): a panel for navigating the application has been created

--- app/src/main/java/ru/foodcare/foodcare/MainActivity.kt
+++ app/src/main/java/ru/foodcare/foodcare/MainActivity.kt
@@ -3,8 +3,18 @@ package ru.foodcare.foodcare
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.material3.DrawerState
+import androidx.compose.material3.DrawerValue
+import androidx.compose.material3.ModalDrawerSheet
+import androidx.compose.material3.ModalNavigationDrawer
+import androidx.compose.material3.NavigationDrawerItem
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.material3.rememberDrawerState
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.rememberNavController
 import ru.foodcare.foodcare.ui.theme.FoodcareTheme
 
 class MainActivity : ComponentActivity() {
@@ -12,20 +22,58 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             FoodcareTheme {
-                Content()
+                UI()
             }
         }
     }
 }
 
+sealed class Route(val route: String) {
+    object Main: Route("main")
+    object Calendar: Route("calendar")
+    object Products: Route("products")
+}
+
 @Composable
-fun Content() {
+fun UI() {
+    val navPanelState = rememberDrawerState (DrawerValue.Open)
+    val navigationController = rememberNavController()
+
+    NavigationPanel(navPanelState, navigationController) {
+        Content(navPanelState, navigationController)
+    }
+}
+
+@Composable
+fun NavigationPanel(navPanelState: DrawerState, navController: NavHostController, content: @Composable () -> Unit) {
+    val drawerContent = @Composable {
+        ModalDrawerSheet {
+            NavigationDrawerItem({Text("Главная")},
+                navController.currentDestination?.route == Route.Main.route,
+                {navController.navigate(Route.Main.route)})
+            NavigationDrawerItem({Text("Календарь")},
+                navController.currentDestination?.route == Route.Calendar.route,
+                {navController.navigate(Route.Calendar.route)})
+            NavigationDrawerItem({Text("Продукты")},
+                navController.currentDestination?.route == Route.Products.route,
+                {navController.navigate(Route.Products.route)})
+        }
+    }
+
+    ModalNavigationDrawer(drawerContent, drawerState = navPanelState) {
+        content()
+    }
+}
+
+@Composable
+fun Content(navPanelState: DrawerState, navController: NavHostController) {
+
 }
 
 @Preview(showBackground = true)
 @Composable
 fun Preview() {
     FoodcareTheme {
-        Content()
+        UI()
     }
 }
