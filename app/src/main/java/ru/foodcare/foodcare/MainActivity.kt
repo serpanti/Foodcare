d0b2442109893bcd404c3d397da7391ce7a857f7
serpenti <sergeitarabukin@yandex.ru>
2025-08-20 01:39:45 +0300

    feat(Content): added navigation through the top bar

--- app/src/main/java/ru/foodcare/foodcare/MainActivity.kt
+++ app/src/main/java/ru/foodcare/foodcare/MainActivity.kt
@@ -3,18 +3,30 @@ package ru.foodcare.foodcare
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Menu
 import androidx.compose.material3.DrawerState
 import androidx.compose.material3.DrawerValue
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.ModalDrawerSheet
 import androidx.compose.material3.ModalNavigationDrawer
 import androidx.compose.material3.NavigationDrawerItem
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextButton
+import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.rememberDrawerState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import kotlinx.coroutines.launch
 import ru.foodcare.foodcare.ui.theme.FoodcareTheme
 
 class MainActivity : ComponentActivity() {
@@ -36,7 +48,7 @@ sealed class Route(val route: String) {
 
 @Composable
 fun UI() {
-    val navPanelState = rememberDrawerState (DrawerValue.Open)
+    val navPanelState = rememberDrawerState (DrawerValue.Closed)
     val navigationController = rememberNavController()
 
     NavigationPanel(navPanelState, navigationController) {
@@ -65,9 +77,26 @@ fun NavigationPanel(navPanelState: DrawerState, navController: NavHostController
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun Content(navPanelState: DrawerState, navController: NavHostController) {
+    Scaffold(topBar = {
+        TopAppBar({
+            Text("Foodcare")
+        }, navigationIcon =  {
+            val navPanelScope = rememberCoroutineScope()
 
+            IconButton({
+                navPanelScope.launch { navPanelState.open() }
+            }) { Icon(Icons.Default.Menu, "Меню") }
+        })
+    }) {
+        NavHost(navController, Route.Main.route, Modifier.padding(it)) {
+            composable(Route.Main.route) {}
+            composable(Route.Calendar.route) {}
+            composable(Route.Products.route) {}
+        }
+    }
 }
 
 @Preview(showBackground = true)
