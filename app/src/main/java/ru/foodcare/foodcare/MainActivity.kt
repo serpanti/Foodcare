60e66ce1dcf9f8868bfba7dd33d203188ea843fa
serpenti <sergeitarabukin@yandex.ru>
2025-08-21 02:33:04 +0300

    feat(MainActivity Products): Добавлено базовое отбражение карточек ProductCard с данными моделей Product

--- app/src/main/java/ru/foodcare/foodcare/MainActivity.kt
+++ app/src/main/java/ru/foodcare/foodcare/MainActivity.kt
@@ -3,8 +3,29 @@ package ru.foodcare.foodcare
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
+import androidx.compose.animation.slideInVertically
+import androidx.compose.animation.slideOutVertically
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.IntrinsicSize
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.KeyboardArrowUp
 import androidx.compose.material.icons.filled.Menu
 import androidx.compose.material3.DrawerState
 import androidx.compose.material3.DrawerValue
@@ -15,13 +36,21 @@ import androidx.compose.material3.ModalDrawerSheet
 import androidx.compose.material3.ModalNavigationDrawer
 import androidx.compose.material3.NavigationDrawerItem
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.rememberDrawerState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.derivedStateOf
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
 import androidx.navigation.NavOptionsBuilder
 import androidx.navigation.compose.NavHost
@@ -29,6 +58,7 @@ import androidx.navigation.compose.composable
 import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
 import kotlinx.coroutines.launch
+import ru.foodcare.foodcare.domain.Product
 import ru.foodcare.foodcare.ui.theme.FoodcareTheme
 
 class MainActivity : ComponentActivity() {
@@ -109,12 +139,122 @@ fun Content(navPanelState: DrawerState, navController: NavHostController) {
             }) { Icon(Icons.Default.Menu, "Меню") }
         })
     }) {
-        NavHost(navController, Route.Main.route, Modifier.padding(it)) {
+        NavHost(navController, Route.Products.route, Modifier.padding(it)) {
             composable(Route.Main.route) {}
             composable(Route.Calendar.route) {}
-            composable(Route.Products.route) {}
+            composable(Route.Products.route) {Products()}
         }
     }
+}
+
+@Composable
+fun Products() {
+    val products = listOf(Product("Томат", "Домашнее производство",
+        100, Product.Companion.UnitType.Gram,
+        24.0, 1.1, 0.2, 3.8, 1.4), Product("Огурец", "Домашнее производство",
+        100, Product.Companion.UnitType.Gram,
+        14.0, 0.8, 0.1, 2.5, 1.0)) + List (10) { Product("Томат", "Домашнее производство",
+        100, Product.Companion.UnitType.Gram,
+        24.0, 1.1, 0.2, 3.8, 1.4) } // вставить работу с БД
+    val productsSorted = remember(products) {
+        products.sortedWith (
+            compareBy<Product> { it.name }
+                .thenBy { it.production }
+        )
+    }
+
+    val lazyListState = rememberLazyListState()
+    val scope = rememberCoroutineScope()
+
+    Box(modifier = Modifier.fillMaxSize()) {
+        LazyColumn (state = lazyListState) {
+            items(productsSorted.size) { idx ->
+                ProductCard(productsSorted[idx], Modifier.padding(vertical = 5.dp,
+                    horizontal = 5.dp))
+            }
+        }
+
+        val showButton by remember {
+            derivedStateOf { lazyListState.firstVisibleItemIndex >= 4 }
+        }
+        AnimatedVisibility(visible = showButton,
+            modifier = Modifier.align(Alignment.BottomCenter),
+            enter = slideInVertically() + fadeIn(), exit = slideOutVertically() + fadeOut()) {
+            IconButton({
+                scope.launch {
+                    lazyListState.scrollToItem(0)
+                }
+            }) { Icon(Icons.Filled.KeyboardArrowUp, "Переместиться наверх") }
+        }
+    }
+}
+
+@Composable
+fun ProductCard(product: Product, modifier: Modifier = Modifier) {
+    Surface(modifier = modifier, shape = RoundedCornerShape(10.dp),
+        border = BorderStroke(2.dp, Color.Black), shadowElevation = 5.dp) {
+        Column(modifier = Modifier.background(Color.Transparent).padding(10.dp)) {
+            Text("Название: " + product.name)
+            Text("Производитель: " + product.production)
+            Nutrients(product, Modifier.padding(horizontal = 2.dp, vertical = 5.dp))
+            Row(modifier = Modifier.align(Alignment.End)) {
+                Text("Кол-во: ")
+                val countTypeString = when (product.type) {
+                    Product.Companion.UnitType.Milliliter -> "мл"
+                    Product.Companion.UnitType.Gram -> "г"
+                    Product.Companion.UnitType.Piece -> "шт"
+                }
+                Text("${product.amount} $countTypeString")
+            }
+        }
+    }
+}
+
+@Composable
+fun Nutrients(product: Product, modifier: Modifier = Modifier) {
+    Column(modifier) {
+        NutrientsHorizontalDivider()
+        Row(modifier = Modifier.fillMaxWidth().height(IntrinsicSize.Min),
+            horizontalArrangement = Arrangement.SpaceBetween) {
+            NutrientsVerticalDivider()
+            Nutrient("Ценность", product.calories, "ккал")
+            NutrientsVerticalDivider()
+            Nutrient("Б", product.protein, "грамм")
+            NutrientsVerticalDivider()
+            Nutrient("Ж", product.fat, "грамм")
+            NutrientsVerticalDivider()
+            Nutrient("У", product.carbohydrates, "грамм")
+            NutrientsVerticalDivider()
+            Nutrient("Волокна", product.fiber, "грамм")
+            NutrientsVerticalDivider()
+        }
+        NutrientsHorizontalDivider()
+    }
+}
+
+@Composable
+fun Nutrient(description: String, value: Double, type: String) {
+    Column(horizontalAlignment = Alignment.CenterHorizontally) {
+        Text(description)
+        Text(value.toString())
+        Text(type)
+    }
+}
+
+@Composable
+fun NutrientsHorizontalDivider() {HorizontalDivider(2.dp, Color.Gray)}
+
+@Composable
+fun NutrientsVerticalDivider() {VerticalDivider(2.dp, Color.Gray)}
+
+@Composable
+fun VerticalDivider(width: Dp, color: Color) {
+    Box(modifier = Modifier.width(width).fillMaxHeight().background(color))
+}
+
+@Composable
+fun HorizontalDivider(height: Dp, color: Color) {
+    Box(modifier = Modifier.height(height).fillMaxWidth().background(color))
 }
 
 @Preview(showBackground = true)
