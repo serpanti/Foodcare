8050d1111725aa9be00c7e10f44930ced10c8387
serpenti <sergeitarabukin@yandex.ru>
2025-08-21 12:24:53 +0300

    feat(Product): добавлена Entity product

--- app/src/main/java/ru/foodcare/foodcare/data/product/Product.kt
+++ app/src/main/java/ru/foodcare/foodcare/data/product/Product.kt
@@ -1,4 +1,39 @@
 package ru.foodcare.foodcare.data.product
 
-class Product {
-}
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "products")
+data class Product (
+    @PrimaryKey(autoGenerate = true)
+    @ColumnInfo(name = "id")
+    val id: Int = 0,
+
+    @ColumnInfo(name = "name")
+    val name: String = "",
+
+    @ColumnInfo(name = "production")
+    val production: String = "",
+
+    @ColumnInfo(name = "amount")
+    val amount: Int = 1,
+
+    @ColumnInfo(name = "type")
+    val type: String = "шт",
+
+    @ColumnInfo(name = "calories")
+    val calories: Double = 0.0,
+
+    @ColumnInfo(name = "protein")
+    val protein: Double = 0.0,
+
+    @ColumnInfo(name = "fat")
+    val fat: Double = 0.0,
+
+    @ColumnInfo(name = "carbohydrates")
+    val carbohydrates: Double = 0.0,
+
+    @ColumnInfo(name = "fiber")
+    val fiber: Double = 0.0
+)
