de07c066bf1f4f54aa0865b0ff7dd08584f5287d
serpenti <sergeitarabukin@yandex.ru>
2025-08-22 06:55:26 +0300

    chore(FoodcareDB): реорганизация папок

--- app/src/main/java/ru/foodcare/foodcare/data/database/FoodcareDB.kt
+++ app/src/main/java/ru/foodcare/foodcare/data/database/FoodcareDB.kt
@@ -1,7 +1,9 @@
-package ru.foodcare.foodcare.data.product
+package ru.foodcare.foodcare.data.database
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
+import ru.foodcare.foodcare.data.product.Product
+import ru.foodcare.foodcare.data.product.ProductDAO
 
 @Database(entities = [Product::class], version = 1)
 abstract class FoodcareDB: RoomDatabase() {
