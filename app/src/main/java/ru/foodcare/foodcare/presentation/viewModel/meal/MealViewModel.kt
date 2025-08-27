a8c5ed13422244cef73e19b2f9ae47766f0f2a92
serpenti <sergeitarabukin@yandex.ru>
2025-08-27 19:02:17 +0300

    feat(ui): холодные данные теперь горячие

--- app/src/main/java/ru/foodcare/foodcare/presentation/viewModel/meal/MealViewModel.kt
+++ app/src/main/java/ru/foodcare/foodcare/presentation/viewModel/meal/MealViewModel.kt
@@ -1,8 +1,12 @@
 package ru.foodcare.foodcare.presentation.viewModel.meal
 
+import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.flowOn
+import kotlinx.coroutines.flow.stateIn
 import kotlinx.coroutines.launch
 import ru.foodcare.foodcare.domain.meal.Meal
 import ru.foodcare.foodcare.domain.meal.MealRepository
@@ -11,20 +15,44 @@ import kotlin.coroutines.CoroutineContext
 open class MealViewModel(private val repository: MealRepository,
     private val context: CoroutineContext): ViewModel() {
 
-    fun observeYears(): Flow<List<Int>> {
-        return repository.observeYears()
+    val observedYears = repository.observeYears()
+        .flowOn(context)
+        .stateIn(
+            scope = viewModelScope,
+            started = SharingStarted.Companion.WhileSubscribed(5000),
+            initialValue = emptyList()
+        )
+
+    var observedYear = mutableStateOf<Int?>(null)
+    var observedMonth = mutableStateOf<Int?>(null)
+    var observedDay = mutableStateOf<Int?>(null)
+    var observedMeal = mutableStateOf<Meal?>(null)
+
+    fun observeMonths(year: Int): StateFlow<List<Int>> {
+        return repository.observeMonths(year).flowOn(context)
+            .stateIn(
+                scope = viewModelScope,
+                started = SharingStarted.Companion.WhileSubscribed(5000),
+                initialValue = emptyList()
+            )
     }
 
-    fun observeMonths(year: Int): Flow<List<Int>> {
-        return repository.observeMonths(year)
+    fun observeDays(year: Int, month: Int): StateFlow<List<Int>> {
+        return repository.observeDays(year, month).flowOn(context)
+            .stateIn(
+                scope = viewModelScope,
+                started = SharingStarted.Companion.WhileSubscribed(5000),
+                initialValue = emptyList()
+            )
     }
 
-    fun observeDays(year: Int, month: Int): Flow<List<Int>> {
-        return repository.observeDays(year, month)
-    }
-
-    fun observeDay(year: Int, month: Int, day: Int): Flow<List<Meal>> {
-        return repository.observeDay(year, month, day)
+    fun observeDay(year: Int, month: Int, day: Int): StateFlow<List<Meal>> {
+        return repository.observeDay(year, month, day).flowOn(context)
+            .stateIn(
+                scope = viewModelScope,
+                started = SharingStarted.Companion.WhileSubscribed(5000),
+                initialValue = emptyList()
+            )
     }
 
     fun addMeal(product: Meal) {
