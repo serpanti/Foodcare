36cb2eaa5dc16ad6f3776c6f5917b03ea39e0fff
serpanti <hidantarabukin@gmail.com>
2025-08-28 17:20:51 +0300

    feat(ui): в TimeEdit написана реализация с использованием кастомного ввода и системного времени

--- app/src/main/java/ru/foodcare/foodcare/presentation/composable/MealEditorWindow.kt
+++ app/src/main/java/ru/foodcare/foodcare/presentation/composable/MealEditorWindow.kt
@@ -17,6 +17,7 @@ import ru.foodcare.foodcare.domain.product.Product
 import ru.foodcare.foodcare.domain.meal.Meal
 import ru.foodcare.foodcare.presentation.input.InputMeal
 import ru.foodcare.foodcare.presentation.viewModel.meal.MealViewModel
+import java.time.LocalDateTime
 
 @Composable
 fun MealEditWindow(viewModel: MealViewModel, close: () -> Unit) {
@@ -76,7 +77,98 @@ fun TimeEdit(
     minutes: MutableState<String>,
     seconds: MutableState<String>
 ) {
+    Column {
+        SquareButton("Установить Текущее время") {
+            setSystemTime(year, month, day, hours, minutes, seconds)
+        }
+        YearTextField(year)
+        MonthTextField(month)
+        DayTextField(day)
+        HoursTextField(hours)
+        MinutesTextField(minutes)
+        SecondsTextField(seconds)
+    }
+}
 
+@Composable
+fun YearTextField(year: MutableState<String>, modifier: Modifier = Modifier) {
+    SimpleTextField(year.value, onValueChange = { newStr ->
+        year.value = newStr
+    }, modifier = modifier, placeholder = {
+        Text("Введите год полностью")
+    }, prefix = {
+        Text("Год: ")
+    })
+}
+
+@Composable
+fun MonthTextField(month: MutableState<String>, modifier: Modifier = Modifier) {
+    SimpleTextField(month.value, onValueChange = { newStr ->
+        month.value = newStr
+    }, modifier = modifier, placeholder = {
+        Text("Введите номер месяца (1-12)")
+    }, prefix = {
+        Text("Месяц: ")
+    })
+}
+
+@Composable
+fun DayTextField(day: MutableState<String>, modifier: Modifier = Modifier) {
+    SimpleTextField(day.value, onValueChange = { newStr ->
+        day.value = newStr
+    }, modifier = modifier, placeholder = {
+        Text("Введите номер дня (1-31)")
+    }, prefix = {
+        Text("День: ")
+    })
+}
+
+@Composable
+fun HoursTextField(hour: MutableState<String>, modifier: Modifier = Modifier) {
+    SimpleTextField(hour.value, onValueChange = { newStr ->
+        hour.value = newStr
+    }, modifier = modifier, placeholder = {
+        Text("Введите номер часа (0-23)")
+    }, prefix = {
+        Text("Часы: ")
+    })
+}
+
+@Composable
+fun MinutesTextField(minutes: MutableState<String>, modifier: Modifier = Modifier) {
+    SimpleTextField(minutes.value, onValueChange = { newStr ->
+        minutes.value = newStr
+    }, modifier = modifier, placeholder = {
+        Text("Введите количество минут (0-59)")
+    }, prefix = {
+        Text("Минуты: ")
+    })
+}
+
+@Composable
+fun SecondsTextField(seconds: MutableState<String>, modifier: Modifier = Modifier) {
+    SimpleTextField(seconds.value, onValueChange = { newStr ->
+        seconds.value = newStr
+    }, modifier = modifier, placeholder = {
+        Text("Введите количество секунд (0-59)")
+    }, prefix = {
+        Text("Секунды: ")
+    })
+}
+
+private fun setSystemTime(year: MutableState<String>,
+                  month: MutableState<String>,
+                  day: MutableState<String>,
+                  hours: MutableState<String>,
+                  minutes: MutableState<String>,
+                  seconds: MutableState<String>) {
+    val time = LocalDateTime.now()
+    year.value = time.year.toString()
+    month.value = time.monthValue.toString()
+    day.value = time.dayOfMonth.toString()
+    hours.value = time.hour.toString()
+    minutes.value = time.minute.toString()
+    seconds.value = time.second.toString()
 }
 
 @Composable
