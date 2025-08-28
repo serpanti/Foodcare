b1fb8d15ed97f251bfaaf0604748379b2d1a869f
serpanti <hidantarabukin@gmail.com>
2025-08-28 17:32:13 +0300

    feat(ui): добавлена проверка существования даты

--- app/src/main/java/ru/foodcare/foodcare/presentation/input/InputMeal.kt
+++ app/src/main/java/ru/foodcare/foodcare/presentation/input/InputMeal.kt
@@ -2,6 +2,8 @@ package ru.foodcare.foodcare.presentation.input
 
 import ru.foodcare.foodcare.domain.meal.Meal
 import ru.foodcare.foodcare.domain.product.Product
+import java.lang.Exception
+import java.time.LocalDateTime
 
 class InputMeal(val id: Int,
                 val product: Product?,
@@ -13,9 +15,16 @@ class InputMeal(val id: Int,
                 val seconds: Int? = null) : Input
 {
     override fun isCorrect(): Boolean {
-        return (year != null && month != null && day != null &&
+        return if (year != null && month != null && day != null &&
                 hours != null && minutes != null && seconds != null
-                && product != null)
+                && product != null) {
+            try {
+                LocalDateTime.of(year, month, day, hours, minutes, seconds)
+                true
+            } catch (_: Exception) {
+                false
+            }
+        } else false
     }
 
     fun toMeal(): Meal {
