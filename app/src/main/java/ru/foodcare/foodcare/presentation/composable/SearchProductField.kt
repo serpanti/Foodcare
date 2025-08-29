7c537658bf601c9c759efc6eef16ed0f391460f1
serpanti <hidantarabukin@gmail.com>
2025-08-29 08:51:12 +0300

    chore(ui): убран ненужный импорт

--- app/src/main/java/ru/foodcare/foodcare/presentation/composable/SearchProductField.kt
+++ app/src/main/java/ru/foodcare/foodcare/presentation/composable/SearchProductField.kt
@@ -2,7 +2,6 @@ package ru.foodcare.foodcare.presentation.composable
 
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.DropdownMenu
-import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
