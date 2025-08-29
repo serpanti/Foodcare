b9ab926d02dcd44cf5f8ad085748c9a4496a0097
serpanti <hidantarabukin@gmail.com>
2025-08-29 08:34:05 +0300

    feat(ui): SearchProductField разнесен на две функции(со списком найденных продуктов и без)

--- app/src/main/java/ru/foodcare/foodcare/presentation/composable/SearchProductField.kt
+++ app/src/main/java/ru/foodcare/foodcare/presentation/composable/SearchProductField.kt
@@ -1,11 +1,21 @@
 package ru.foodcare.foodcare.presentation.composable
 
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.DropdownMenu
+import androidx.compose.material3.DropdownMenuItem
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.derivedStateOf
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.DpOffset
+import ru.foodcare.foodcare.domain.product.Product
 import ru.foodcare.foodcare.presentation.viewModel.product.ProductViewModel
 
 @Composable
-fun SearchProductField(productViewModel: ProductViewModel,
-                       dropDownList: @Composable () -> Unit = {}) {
+fun SearchProductField(productViewModel: ProductViewModel) {
     val startValue = productViewModel.key.value ?: ""
     val endSearch = null
 
@@ -29,7 +39,39 @@ fun SearchProductField(productViewModel: ProductViewModel,
 
     SearchField(onStartSearching, onStopSearching, startValue, onValueChange
     ) { productViewModel.key.value != null }
-    dropDownList()
+}
+
+@Composable
+fun SearchProductFieldWithList(productViewModel: ProductViewModel,
+                                   list: @Composable (visible: Boolean, close: () -> Unit,
+                                                              products: List<Product>?) -> Unit) {
+    val startValue = productViewModel.key.value ?: ""
+    val endSearch = null
+
+    val search: () -> Unit = {
+        productViewModel.key.value?.let {
+            productViewModel.getProductByNameOrProduction(it)
+        }
+    }
+    val onStartSearching: () -> Unit = {
+        productViewModel.key.value = startValue
+        search()
+    }
+    val onStopSearching: () -> Unit = {
+        productViewModel.key.value = endSearch
+        productViewModel.productsStatic.value = endSearch
+    }
+    val onValueChange: (String) -> Unit = {
+        productViewModel.key.value = it
+        search()
+    }
+
+    SearchField(onStartSearching, onStopSearching, startValue, onValueChange
+    ) { productViewModel.key.value != null }
+
+    val products = remember { productViewModel.productsStatic }
+    val visible by remember { derivedStateOf { productViewModel.key.value != null } }
+    list(visible, onStopSearching, products.value)
 }
 
 @Composable
