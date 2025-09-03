7f6527c7023ca1c004663b6b0d5280d6bd446005
serpanti <hidantarabukin@gmail.com>
2025-09-03 17:44:05 +0300

    feat(bd): создан маппер для веса

---
+++ app/src/main/java/ru/foodcare/foodcare/data/weightMeasurement/WeightMeasurementMapper.kt
@@ -0,0 +1,28 @@
+package ru.foodcare.foodcare.data.weightMeasurement
+
+import ru.foodcare.foodcare.data.date.DateMapper
+import ru.foodcare.foodcare.domain.weight.Weight
+
+object WeightMeasurementMapper {
+    fun toDomain(dateWithWeightMeasurements: DateWithWeightMeasurements): List<Weight> {
+        return dateWithWeightMeasurements.weightMeasurements.map { weightMeasurement ->
+            Weight(
+                id = weightMeasurement.id,
+                value = weightMeasurement.weight,
+                date = DateMapper.toDomain(dateWithWeightMeasurements.date)
+                    .atTime(weightMeasurement.hours,
+                        weightMeasurement.minutes,
+                        weightMeasurement.seconds)
+            )
+        }
+    }
+
+    fun fromDomain(weight: Weight, dateId: Int): WeightMeasurement = WeightMeasurement(
+        id = weight.id,
+        dateId = dateId,
+        hours = weight.date.hour,
+        minutes = weight.date.minute,
+        seconds = weight.date.second,
+        weight = weight.value,
+    )
+}
