648ea71ce595976e96a826a85da8c1eac6c5b169
serpanti <hidantarabukin@gmail.com>
2025-09-03 18:21:48 +0300

    feat(ui): viewModel для веса подключена в активити

--- app/src/main/java/ru/foodcare/foodcare/MainActivity.kt
+++ app/src/main/java/ru/foodcare/foodcare/MainActivity.kt
@@ -15,6 +15,7 @@ import ru.foodcare.foodcare.data.database.FoodcareDBProvider
 import ru.foodcare.foodcare.data.date.DateRepositoryImpl
 import ru.foodcare.foodcare.data.meal.MealRepositoryImpl
 import ru.foodcare.foodcare.data.product.ProductRepositoryImpl
+import ru.foodcare.foodcare.data.weightMeasurement.WeightRepositoryImpl
 import ru.foodcare.foodcare.presentation.composable.Content
 import ru.foodcare.foodcare.presentation.composable.NavigationPanel
 import ru.foodcare.foodcare.presentation.mock.MockMealViewModel
@@ -25,6 +26,8 @@ import ru.foodcare.foodcare.presentation.viewModel.meal.MealViewModel
 import ru.foodcare.foodcare.presentation.viewModel.meal.MealViewModelFactory
 import ru.foodcare.foodcare.presentation.viewModel.product.ProductViewModel
 import ru.foodcare.foodcare.presentation.viewModel.product.ProductViewModelFactory
+import ru.foodcare.foodcare.presentation.viewModel.weight.WeightViewModel
+import ru.foodcare.foodcare.presentation.viewModel.weight.WeightViewModelFactory
 import ru.foodcare.foodcare.ui.theme.FoodcareTheme
 
 class MainActivity : ComponentActivity() {
@@ -32,6 +35,7 @@ class MainActivity : ComponentActivity() {
     private lateinit var productVM: ProductViewModel
     private lateinit var mealVM: MealViewModel
     private lateinit var dateVM: DateViewModel
+    private lateinit var weightVM: WeightViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -41,14 +45,17 @@ class MainActivity : ComponentActivity() {
         val productRepository = ProductRepositoryImpl(db.productDAO())
         val mealRepository = MealRepositoryImpl(db.mealDAO(), db.productDAO(), db.dateDAO())
         val dateRepository = DateRepositoryImpl(db.dateDAO())
+        val weightRepository = WeightRepositoryImpl(db.weightMeasurementDAO(), db.dateDAO())
 
         val productFactory = ProductViewModelFactory(productRepository, Dispatchers.IO)
         val mealFactory = MealViewModelFactory(mealRepository, Dispatchers.IO)
         val dateFactory = DateViewModelFactory(dateRepository, Dispatchers.IO)
+        val weightFactory = WeightViewModelFactory(weightRepository, Dispatchers.IO)
 
         productVM = ViewModelProvider(this, productFactory)[ProductViewModel::class.java]
         mealVM = ViewModelProvider(this, mealFactory)[MealViewModel::class.java]
         dateVM = ViewModelProvider(this, dateFactory)[DateViewModel::class.java]
+        weightVM = ViewModelProvider(this, weightFactory)[WeightViewModel::class.java]
 
         setContent {
             FoodcareTheme {
