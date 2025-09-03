824ceace9c3025514f1483256161f0e6ff27f73d
serpanti <hidantarabukin@gmail.com>
2025-09-03 22:48:48 +0300

    feat(ui): добавлены функции и переменные для отслеживания веса доступного к редактированию

--- app/src/main/java/ru/foodcare/foodcare/presentation/viewModel/weight/WeightViewModel.kt
+++ app/src/main/java/ru/foodcare/foodcare/presentation/viewModel/weight/WeightViewModel.kt
@@ -2,8 +2,10 @@ package ru.foodcare.foodcare.presentation.viewModel.weight
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.flowOn
 import kotlinx.coroutines.flow.stateIn
 import kotlinx.coroutines.launch
@@ -14,6 +16,17 @@ import kotlin.coroutines.CoroutineContext
 
 class WeightViewModel(private val weightRepository: WeightRepository,
                       private val context: CoroutineContext) : ViewModel() {
+    private val _weightObserved: MutableStateFlow<Weight?> = MutableStateFlow(null)
+    val weightObserved: StateFlow<Weight?> = _weightObserved.asStateFlow()
+
+    fun onUpdateWeight(weight: Weight) {
+        _weightObserved.value = weight
+    }
+
+    fun onAddWeight() {
+        _weightObserved.value = null
+    }
+                          
     fun observeDay(date: LocalDate): StateFlow<List<Weight>> {
         return weightRepository.observeDay(date.year, date.monthValue, date.dayOfMonth)
             .flowOn(context)
