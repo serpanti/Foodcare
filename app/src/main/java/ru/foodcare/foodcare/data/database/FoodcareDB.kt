3f0ca12a0cd408485080a301806db66a6d5acdcc
serpanti <hidantarabukin@gmail.com>
2025-09-03 17:44:38 +0300

    feat(bd): вес добавлен в FoodcareDB

--- app/src/main/java/ru/foodcare/foodcare/data/database/FoodcareDB.kt
+++ app/src/main/java/ru/foodcare/foodcare/data/database/FoodcareDB.kt
@@ -8,11 +8,14 @@ import ru.foodcare.foodcare.data.meal.Meal
 import ru.foodcare.foodcare.data.meal.MealDAO
 import ru.foodcare.foodcare.data.product.Product
 import ru.foodcare.foodcare.data.product.ProductDAO
+import ru.foodcare.foodcare.data.weightMeasurement.WeightMeasurement
+import ru.foodcare.foodcare.data.weightMeasurement.WeightMeasurementDAO
 
-@Database(entities = [Product::class, Meal::class, Date::class],
+@Database(entities = [Product::class, Meal::class, Date::class, WeightMeasurement::class],
     version = 1)
 abstract class FoodcareDB: RoomDatabase() {
     abstract fun productDAO(): ProductDAO
     abstract fun mealDAO(): MealDAO
     abstract fun dateDAO(): DateDAO
+    abstract fun weightMeasurementDAO(): WeightMeasurementDAO
 }
