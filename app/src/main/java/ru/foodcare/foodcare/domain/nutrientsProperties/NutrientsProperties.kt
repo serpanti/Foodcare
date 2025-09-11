afddd69f6c5b6c0eecacf298475d2d9fcda085cc
serpanti <hidantarabukin@gmail.com>
2025-09-11 03:40:42 +0300

    feat(domain): создан класс для хранения состава пищи

---
+++ app/src/main/java/ru/foodcare/foodcare/domain/nutrientsProperties/NutrientsProperties.kt
@@ -0,0 +1,9 @@
+package ru.foodcare.foodcare.domain.nutrientsProperties
+
+data class NutrientsProperties(
+    val calories: Double,
+    val protein: Double,
+    val fat: Double,
+    val carbohydrates: Double,
+    val fiber: Double
+)
