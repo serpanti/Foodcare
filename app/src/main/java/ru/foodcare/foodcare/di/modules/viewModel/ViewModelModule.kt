3916718d9dd94739af9af5999f37b8a0c4802f69
serpanti <hidantarabukin@gmail.com>
2025-09-17 01:59:37 +0300

    feat(di): написаны ключ для viewModel map фабрики и модуль для viewModel

---
+++ app/src/main/java/ru/foodcare/foodcare/di/modules/viewModel/ViewModelModule.kt
@@ -0,0 +1,33 @@
+package ru.foodcare.foodcare.di.modules.viewModel
+
+import androidx.lifecycle.ViewModel
+import dagger.Binds
+import dagger.Module
+import dagger.multibindings.IntoMap
+import ru.foodcare.foodcare.ui.viewModel.date.DateViewModel
+import ru.foodcare.foodcare.ui.viewModel.meal.MealViewModel
+import ru.foodcare.foodcare.ui.viewModel.product.ProductViewModel
+import ru.foodcare.foodcare.ui.viewModel.weight.WeightViewModel
+
+@Module
+interface ViewModelModule {
+    @Binds
+    @IntoMap
+    @ViewModelKey(ProductViewModel::class)
+    fun bindsProductViewModel(productViewModel: ProductViewModel): ViewModel
+
+    @Binds
+    @IntoMap
+    @ViewModelKey(MealViewModel::class)
+    fun bindsMealViewModel(mealViewModel: MealViewModel): ViewModel
+
+    @Binds
+    @IntoMap
+    @ViewModelKey(DateViewModel::class)
+    fun bindsDateViewModel(dateViewModel: DateViewModel): ViewModel
+
+    @Binds
+    @IntoMap
+    @ViewModelKey(WeightViewModel::class)
+    fun bindsWeightViewModel(weightViewModel: WeightViewModel): ViewModel
+}
