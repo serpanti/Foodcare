dc1032d7ccd718c32a10e73e77ed23d8b0965394
serpanti <hidantarabukin@gmail.com>
2025-09-20 14:57:24 +0300

    feat(ui): в NutrientsPropertiesCard заголовок приведен к стилю Material3

--- app/src/main/java/ru/foodcare/foodcare/ui/composable/nutrients/Nutrients.kt
+++ app/src/main/java/ru/foodcare/foodcare/ui/composable/nutrients/Nutrients.kt
@@ -7,13 +7,13 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.material3.VerticalDivider
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.unit.sp
 import ru.foodcare.foodcare.R
 import ru.foodcare.foodcare.domain.nutrientsProperties.NutrientsProperties
 import ru.foodcare.foodcare.ui.composable.ElementWithHeader
@@ -87,7 +87,8 @@ fun NutrientsVerticalDivider() {
 fun NutrientsPropertiesCard(nutrientsProperties: NutrientsProperties,
                             modifier: Modifier = Modifier) {
     ElementWithHeader(header = {
-        Text(stringResource(R.string.nutrients_sum_label), fontSize = 24.sp)
+        Text(stringResource(R.string.nutrients_sum_label),
+            style = MaterialTheme.typography.titleLarge)
     }, modifier = modifier) {
         Nutrients(nutrientsProperties, modifier)
     }
