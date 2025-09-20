b88354884dc2b9777e69f2aa29f262c87e2a9c96
serpanti <hidantarabukin@gmail.com>
2025-09-20 22:51:47 +0300

    fix(ui): изменен виджет отображения нутриентов
    Теперь нутриенты отображаются в двух строках по три элемента в каждой.
    Каждый элемент имеет рамку и внутренний отступ.
    Удалены разделители между элементами.

--- app/src/main/java/ru/foodcare/foodcare/ui/composable/nutrients/Nutrients.kt
+++ app/src/main/java/ru/foodcare/foodcare/ui/composable/nutrients/Nutrients.kt
@@ -1,15 +1,15 @@
 package ru.foodcare.foodcare.ui.composable.nutrients
 
-import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.IntrinsicSize
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
-import androidx.compose.material3.HorizontalDivider
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
-import androidx.compose.material3.VerticalDivider
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -18,46 +18,49 @@ import ru.foodcare.foodcare.R
 import ru.foodcare.foodcare.domain.nutrientsProperties.NutrientsProperties
 import ru.foodcare.foodcare.ui.composable.ElementWithHeader
 import androidx.compose.ui.platform.LocalResources
+import androidx.compose.ui.unit.dp
 
 @Composable
 fun Nutrients(nutrientsProperties: NutrientsProperties, modifier: Modifier = Modifier) {
     val res = LocalResources.current
 
-    Column(modifier) {
-        NutrientsHorizontalDivider()
-        Row(modifier = Modifier
-            .fillMaxWidth()
-            .height(IntrinsicSize.Min),
-            horizontalArrangement = Arrangement.SpaceBetween) {
-            NutrientsVerticalDivider()
-            Nutrient(stringResource(R.string.caloric_content),
-                nutrientsProperties.calories.toInt(), stringResource(R.string.calories_short))
-            NutrientsVerticalDivider()
+    Column(modifier = modifier
+        .fillMaxWidth(),
+        horizontalAlignment = Alignment.CenterHorizontally) {
+        Row(Modifier.height(IntrinsicSize.Max).fillMaxWidth()) {
             Nutrient(stringResource(R.string.protein_short_capital),
                 nutrientsProperties.protein,
                 res.getQuantityString(R.plurals.weight_in_grams,
-                    nutrientsProperties.protein.toInt()))
-            NutrientsVerticalDivider()
+                    nutrientsProperties.protein.toInt()),
+                modifier = Modifier.weight(1f))
             Nutrient(stringResource(R.string.fat_short_capital),
                 nutrientsProperties.fat, res.getQuantityString(R.plurals.weight_in_grams,
-                    nutrientsProperties.fat.toInt()))
-            NutrientsVerticalDivider()
+                    nutrientsProperties.fat.toInt()),
+                modifier = Modifier.weight(1f))
             Nutrient(stringResource(R.string.carbohydrates_short_capital),
                 nutrientsProperties.carbohydrates, res.getQuantityString(R.plurals.weight_in_grams,
-                    nutrientsProperties.carbohydrates.toInt()))
-            NutrientsVerticalDivider()
+                    nutrientsProperties.carbohydrates.toInt()),
+                modifier = Modifier.weight(1f))
+        }
+        Row(Modifier.height(IntrinsicSize.Max).fillMaxWidth()) {
+            Nutrient(stringResource(R.string.caloric_content),
+                nutrientsProperties.calories.toInt(), stringResource(R.string.calories_short),
+                modifier = Modifier.weight(1f))
             Nutrient(stringResource(R.string.fibers_capital),
                 nutrientsProperties.fiber, res.getQuantityString(R.plurals.weight_in_grams,
-                    nutrientsProperties.fiber.toInt()))
-            NutrientsVerticalDivider()
+                    nutrientsProperties.fiber.toInt()),
+                modifier = Modifier.weight(1f))
         }
-        NutrientsHorizontalDivider()
     }
 }
 
 @Composable
-fun Nutrient(description: String, value: Double, type: String) {
-    Column(horizontalAlignment = Alignment.CenterHorizontally) {
+fun Nutrient(description: String, value: Double, type: String, modifier: Modifier = Modifier) {
+    Column(horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = modifier
+            .fillMaxHeight()
+            .border(1.dp, MaterialTheme.colorScheme.outline)
+            .padding(10.dp)) {
         Text(description)
         Text("%.2f".format(value))
         Text(type)
@@ -65,22 +68,16 @@ fun Nutrient(description: String, value: Double, type: String) {
 }
 
 @Composable
-fun Nutrient(description: String, value: Int, type: String) {
-    Column(horizontalAlignment = Alignment.CenterHorizontally) {
+fun Nutrient(description: String, value: Int, type: String, modifier: Modifier = Modifier) {
+    Column(horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = modifier
+            .fillMaxHeight()
+            .border(1.dp, MaterialTheme.colorScheme.outline)
+            .padding(10.dp)) {
         Text(description)
         Text("$value")
         Text(type)
     }
-}
-
-@Composable
-fun NutrientsHorizontalDivider() {
-    HorizontalDivider()
-}
-
-@Composable
-fun NutrientsVerticalDivider() {
-    VerticalDivider()
 }
 
 @Composable
