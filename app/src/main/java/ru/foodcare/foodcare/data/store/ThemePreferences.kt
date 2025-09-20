581382d024c0ebf2f6d284d586abf23c8a105aa3
serpanti <hidantarabukin@gmail.com>
2025-09-20 21:47:20 +0300

    feat(data/store): добавлен класс ThemePreferences для сохранения настроек темы
    Добавлен класс `ThemePreferences` для управления сохранением и загрузкой настроек темы приложения.
    Используется `DataStore` для персистентного хранения выбранной темы.
    Доступны следующие варианты тем: светлая, темная и системная.

---
+++ app/src/main/java/ru/foodcare/foodcare/data/store/ThemePreferences.kt
@@ -0,0 +1,41 @@
+package ru.foodcare.foodcare.data.store
+
+import android.content.Context
+import androidx.datastore.preferences.core.edit
+import androidx.datastore.preferences.core.stringPreferencesKey
+import androidx.datastore.preferences.preferencesDataStore
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
+
+val Context.dataStore by preferencesDataStore(name = "settings")
+
+private val THEME_KEY = stringPreferencesKey("theme")
+
+data class ThemePreferences(private val context: Context) {
+    val theme: Flow<String> = context.dataStore.data
+        .map { prefs -> prefs[THEME_KEY] ?: Themes.SYSTEM.toString() }
+
+    suspend fun setDarkTheme() {
+        context.dataStore.edit { prefs ->
+            prefs[THEME_KEY] = Themes.DARK.toString()
+        }
+    }
+
+    suspend fun setLightTheme() {
+        context.dataStore.edit { prefs ->
+            prefs[THEME_KEY] = Themes.LIGHT.toString()
+        }
+    }
+
+    suspend fun setSystemTheme() {
+        context.dataStore.edit { prefs ->
+            prefs[THEME_KEY] = Themes.SYSTEM.toString()
+        }
+    }
+
+    companion object {
+        enum class Themes {
+            LIGHT, DARK, SYSTEM
+        }
+    }
+}
