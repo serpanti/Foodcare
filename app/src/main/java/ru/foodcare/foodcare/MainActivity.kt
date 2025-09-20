0d105d728b9016539e1f398116a68fbfdf3d409e
serpanti <hidantarabukin@gmail.com>
2025-09-20 16:06:26 +0300

    feat(ui): включен edge-to-edge режим в MainActivity

--- app/src/main/java/ru/foodcare/foodcare/MainActivity.kt
+++ app/src/main/java/ru/foodcare/foodcare/MainActivity.kt
@@ -3,6 +3,7 @@ package ru.foodcare.foodcare
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
 import androidx.compose.material3.DrawerValue
 import androidx.compose.material3.rememberDrawerState
 import androidx.compose.runtime.Composable
@@ -23,6 +24,7 @@ class MainActivity : ComponentActivity() {
     private lateinit var weightVM: WeightViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        enableEdgeToEdge()
         super.onCreate(savedInstanceState)
 
         val foodcareComponent = application.foodcare.foodcareComponent
