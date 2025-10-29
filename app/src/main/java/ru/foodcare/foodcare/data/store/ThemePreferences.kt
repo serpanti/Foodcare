66675c5b94fdcf29df15755db35ca7881d4f45fe
serpanti <hidantarabukin@gmail.com>
2025-10-29 08:24:53 +0300

    feat(di): внедрена зависимость ThemePreferences через Dagger

--- app/src/main/java/ru/foodcare/foodcare/data/store/ThemePreferences.kt
+++ app/src/main/java/ru/foodcare/foodcare/data/store/ThemePreferences.kt
@@ -6,12 +6,13 @@ import androidx.datastore.preferences.core.stringPreferencesKey
 import androidx.datastore.preferences.preferencesDataStore
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.map
+import javax.inject.Inject
 
 val Context.dataStore by preferencesDataStore(name = "settings")
 
 private val THEME_KEY = stringPreferencesKey("theme")
 
-data class ThemePreferences(private val context: Context) {
+data class ThemePreferences @Inject constructor(private val context: Context) {
     val theme: Flow<String> = context.dataStore.data
         .map { prefs -> prefs[THEME_KEY] ?: Themes.SYSTEM.toString() }
 
