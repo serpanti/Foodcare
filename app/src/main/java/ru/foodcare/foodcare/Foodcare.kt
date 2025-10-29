09bb22a20ba2a912b71065287ac877dd95be4aa3
serpanti <hidantarabukin@gmail.com>
2025-10-29 08:28:39 +0300

    feat(app): добавлен метод для перезапуска приложения
    В класс `Foodcare` добавлен метод `restart()`, который выполняет перезапуск приложения. Для этого используется `AlarmManager` для планирования немедленного запуска `PendingIntent` с флагами `FLAG_ACTIVITY_CLEAR_TOP` и `FLAG_ACTIVITY_NEW_TASK`. После установки "будильника" текущий процесс завершается с помощью `exitProcess(0)`.

--- app/src/main/java/ru/foodcare/foodcare/Foodcare.kt
+++ app/src/main/java/ru/foodcare/foodcare/Foodcare.kt
@@ -1,9 +1,13 @@
 package ru.foodcare.foodcare
 
+import android.app.AlarmManager
 import android.app.Application
+import android.app.PendingIntent
 import android.content.Context
+import android.content.Intent
 import ru.foodcare.foodcare.di.components.DaggerFoodcareComponent
 import ru.foodcare.foodcare.di.components.FoodcareComponent
+import kotlin.system.exitProcess
 
 class Foodcare: Application() {
     lateinit var foodcareComponent: FoodcareComponent
@@ -12,6 +16,27 @@ class Foodcare: Application() {
     override fun onCreate() {
         super.onCreate()
         foodcareComponent = DaggerFoodcareComponent.factory().create(this)
+    }
+
+    fun restart() {
+        val intent = packageManager.getLaunchIntentForPackage(packageName)
+        intent?.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP or Intent.FLAG_ACTIVITY_NEW_TASK)
+
+        val pendingIntent = PendingIntent.getActivity(
+            this,
+            0,
+            intent,
+            PendingIntent.FLAG_CANCEL_CURRENT or PendingIntent.FLAG_IMMUTABLE
+        )
+
+        val alarmManager = getSystemService(ALARM_SERVICE) as AlarmManager
+        alarmManager.set(
+            AlarmManager.RTC,
+            System.currentTimeMillis() + 100,
+            pendingIntent
+        )
+
+        exitProcess(0)
     }
 }
 
