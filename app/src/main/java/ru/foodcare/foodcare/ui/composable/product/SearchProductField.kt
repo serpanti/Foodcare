4e229c1634be8ace357989d1785a09bfdd38c6f6
serpanti <hidantarabukin@gmail.com>
2025-11-29 10:27:25 +0300

    chore(ui): в SearchProductFieldWithList deprecated MenuAnchorType заменен на ExposedDropdownMenuAnchorType

--- app/src/main/java/ru/foodcare/foodcare/ui/composable/product/SearchProductField.kt
+++ app/src/main/java/ru/foodcare/foodcare/ui/composable/product/SearchProductField.kt
@@ -6,7 +6,7 @@ import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.ExposedDropdownMenuBox
 import androidx.compose.material3.ExposedDropdownMenuDefaults
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.MenuAnchorType
+import androidx.compose.material3.ExposedDropdownMenuAnchorType
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
@@ -67,7 +67,7 @@ fun SearchProductFieldWithList(
         onExpandedChange = {}) {
         TextField(modifier = modifier
             .fillMaxWidth()
-            .menuAnchor(MenuAnchorType.PrimaryEditable),
+            .menuAnchor(ExposedDropdownMenuAnchorType.PrimaryEditable),
             value = key.value,
             onValueChange = onValueChange,
             singleLine = true,
